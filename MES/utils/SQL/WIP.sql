e6a2adbd13bb199182dc63cb714b2f1ffc89d148
SCAN_Naphat NoomthongOEM <naphat-noo@snc-former.com>
2025-07-02 15:36:13 +0700

    Add IN Tap in addPartModal 15:36 02/07/25

---
+++ MES/utils/SQL/WIP.sql
@@ -0,0 +1,18 @@
+IF OBJECT_ID('dbo.WIP_ENTRIES', 'U') IS NULL
+BEGIN
+    PRINT 'Creating WIP_ENTRIES table...';
+    CREATE TABLE [dbo].[WIP_ENTRIES](
+        [entry_id] [int] IDENTITY(1,1) NOT NULL,
+        [entry_time] [datetime] NOT NULL,
+        [line] [varchar](50) NOT NULL,
+        [lot_no] [nvarchar](100) NULL,
+        [part_no] [varchar](50) NOT NULL,
+        [quantity_in] [int] NOT NULL,
+        [operator] [varchar](100) NOT NULL,
+        [remark] [nvarchar](255) NULL,
+        CONSTRAINT [PK_WIP_ENTRIES] PRIMARY KEY CLUSTERED ([entry_id] ASC)
+    );
+    ALTER TABLE [dbo].[WIP_ENTRIES] ADD CONSTRAINT [DF_WIP_entry_time] DEFAULT (getdate()) FOR [entry_time];
+    PRINT 'Table WIP_ENTRIES created successfully.';
+END
+GO
