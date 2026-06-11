a0731d95b2cc1ba5b0f8a83e9359ad3df52057e4
Scyst <napat33025@gmail.com>
2026-06-11 11:28:00 +0700

    Clean up remaining test_shift.php

--- MES/page/manpower/test_shift.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'db.php';
-$stmtShifts = $pdo->query("SELECT shift_id, start_time FROM dbo.MANPOWER_SHIFTS");
-$res = $stmtShifts->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($res);
