69b9ba447a6ed774a2fca348e602b96cac84b38f
Scyst <napat33025@gmail.com>
2026-07-24 11:28:46 +0700

    chore: remove one-off scripts and backup files

--- MES/temp_cols.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/page/db.php';
-$stmt = $pdo->query("SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'MT_ITEMS'");
-$cols = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r($cols);
